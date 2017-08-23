102b9e2ed9a12c8e031fa6e4a4d3b4b919af740c
houshengluan <houshengluan1989@163.com>
2017-08-23 14:15:16 +0800

    add wow

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("done");
+        System.out.println("wow");
+        System.out.println("wow");
+
     }
 }
